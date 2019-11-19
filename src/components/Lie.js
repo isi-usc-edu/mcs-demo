22db38bb44e957c7c609bb03f787740b665d076e
g1eb <mail@g1eb.com>
2019-11-19 13:01:08 -0800

    Add a clearer indicator of a lie to the lie component

--- src/components/Lie.js
+++ src/components/Lie.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import CancelIcon from '@material-ui/icons/Cancel'
+import Typography from '@material-ui/core/Typography'
 import { withStyles } from '@material-ui/core/styles'
 
 
@@ -10,6 +11,7 @@ const styles = theme => ({
     fontSize: theme.spacing(15),
     right: theme.spacing(3),
     top: theme.spacing(3),
+    zIndex: 5,
   },
   shadowIcon: {
     color: 'darkred',
@@ -17,6 +19,17 @@ const styles = theme => ({
     fontSize: theme.spacing(15),
     right: theme.spacing(3),
     top: theme.spacing(3.5),
+    zIndex: 4,
+  },
+  h1: {
+    color: 'red',
+    position: 'absolute',
+    fontSize: theme.spacing(5),
+    right: theme.spacing(19),
+    top: theme.spacing(8),
+    fontWeight: 'bolder',
+    textShadow: '2px 2px 0px darkred',
+    zIndex: 6,
   },
 })
 
@@ -27,6 +40,7 @@ class Lie extends React.Component {
     const { classes } = this.props
     return (
       <span>
+        <Typography component="h1" variant="h4" className={classes.h1}>LIE!</Typography>
         <CancelIcon className={classes.shadowIcon} />
         <CancelIcon className={classes.cancelIcon} />
       </span>
