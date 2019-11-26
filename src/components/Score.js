9e11ddbf45a0ccfb5c063c846ae5129bf3ad4287
g1eb <mail@g1eb.com>
2019-11-26 14:00:50 -0800

    Impprove responsiveness of the output scores in the mobile view

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Typography } from '@material-ui/core/'
 import { withStyles } from '@material-ui/core/styles'
 import Truth from './Truth'
 import Lie from './Lie'
@@ -7,19 +6,24 @@ import { colors } from '../utils/colors'
 
 
 const styles = theme => ({
-  h5: {
-    width: '100%',
-    color: '#fefefe',
-    textAlign: 'left',
-    marginTop: '1em',
+  score: {
+    position: 'relative',
     cursor: 'pointer',
+    marginTop: theme.spacing(2),
+    color: '#fefefe',
+    width: '100%',
+    '@media (min-width:600px)': {
+      marginTop: theme.spacing(3),
+    },
   },
   label: {
     opacity: 0.85,
-  },
-  score: {
-    position: 'relative',
-    width: '94%',
+    textAlign: 'left',
+    fontSize: theme.spacing(2),
+    '@media (min-width:600px)': {
+      marginTop: theme.spacing(3),
+      fontSize: theme.spacing(3),
+    },
   },
   scoreBar: {
     position: 'relative',
@@ -34,8 +38,8 @@ const styles = theme => ({
   },
   outputLabels: {
     position: 'absolute',
-    right: -1 * theme.spacing(10),
     top: theme.spacing(2),
+    right: 0,
   },
 })
 
@@ -45,21 +49,19 @@ class Score extends React.Component {
     const { classes } = this.props
     const { statement } = this.props
     return Object.keys(statement.output).map((systemId, i) => (
-      <Typography component="h5" variant="h5" className={classes.h5} key={systemId}>
-        <div title={statement.output[systemId].prob} className={classes.score}>
-          <label className={classes.label}>
-            {systemId} (probability of truth: {statement.output[systemId].prob.toFixed(2)}%, score: {statement.output[systemId].score.toFixed(2)})
-          </label>
-          <div className={classes.scoreBar}
-            style={{'width': `${statement.output[systemId].prob.toFixed(2)}%`, 'background': colors[i]}}>
-            <span className={classes.scoreLabel}>{statement.output[systemId].prob.toFixed(2)}%</span>
-          </div>
-          <div className={classes.outputLabels}>
-            {!!statement.output[systemId].lie && <Lie />}
-            {!statement.output[systemId].lie && <Truth />}
-          </div>
+      <div className={classes.score} key={systemId}>
+        <label title={statement.output[systemId].prob} className={classes.label}>
+          {systemId} (probability of truth: {statement.output[systemId].prob.toFixed(2)}%, score: {statement.output[systemId].score.toFixed(2)})
+        </label>
+        <div className={classes.scoreBar}
+          style={{'width': `${statement.output[systemId].prob.toFixed(2)}%`, 'background': colors[i]}}>
+          <span className={classes.scoreLabel}>{statement.output[systemId].prob.toFixed(2)}%</span>
         </div>
-      </Typography>
+        <div className={classes.outputLabels}>
+          {!!statement.output[systemId].lie && <Lie />}
+          {!statement.output[systemId].lie && <Truth />}
+        </div>
+      </div>
     ))
   }
 }
