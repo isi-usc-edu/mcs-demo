de962a736d501731a92a0b72e2589b6a3f505242
g1eb <mail@g1eb.com>
2020-01-27 08:15:32 -0800

    Remove the various score bars from the machine output component

--- src/components/Output.js
+++ src/components/Output.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import Truth from './Truth'
 import Lie from './Lie'
-import { colors } from '../utils/colors'
 
 
 const styles = theme => ({
@@ -13,26 +12,6 @@ const styles = theme => ({
     '@media (min-width:600px)': {
     },
   },
-  label: {
-    opacity: 0.85,
-    textAlign: 'left',
-    fontSize: theme.spacing(2),
-    '@media (min-width:600px)': {
-      marginTop: theme.spacing(3),
-      fontSize: theme.spacing(3),
-    },
-  },
-  scoreBar: {
-    position: 'relative',
-    height: theme.spacing(2),
-    minWidth: '0.5%',
-  },
-  scoreLabel: {
-    position: 'absolute',
-    fontSize: theme.spacing(2),
-    marginTop: -1 * theme.spacing(0.25),
-    right: -1 * theme.spacing(7.5),
-  },
   outputLabels: {
     position: 'absolute',
     top: -1 * theme.spacing(3),
@@ -42,24 +21,22 @@ const styles = theme => ({
 
 class Output extends React.Component {
 
+  findTruth(statement) {
+    return true
+  }
+
   render() {
     const { classes } = this.props
     const { statement } = this.props
-    return Object.keys(statement.output).map((systemId, i) => (
-      <div className={classes.score} key={systemId}>
-        <label title={statement.output[systemId].prob} className={classes.label}>
-          {systemId} (probability of truth: {statement.output[systemId].prob.toFixed(2)}%, score: {statement.output[systemId].score.toFixed(2)})
-        </label>
-        <div className={classes.scoreBar}
-          style={{'width': `${statement.output[systemId].prob.toFixed(2)}%`, 'background': colors[i]}}>
-          <span className={classes.scoreLabel}>{statement.output[systemId].prob.toFixed(2)}%</span>
-        </div>
+    let truth = this.findTruth(statement)
+    return (
+      <div className={classes.score}>
         <div className={classes.outputLabels}>
-          {!!statement.output[systemId].lie && <Lie />}
-          {!statement.output[systemId].lie && <Truth />}
+          {truth && <Truth />}
+          {!truth && <Lie />}
         </div>
       </div>
-    ))
+    )
   }
 }
 
